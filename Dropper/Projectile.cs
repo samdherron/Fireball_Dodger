3b067a0e581a41bf182f15a17b30f604d1f974f8
Sam Herron <samdherron@gmail.com>
2019-07-16 21:02:08 -0400

    Added a new collection of edited fireball images. Animated them to make the fireball glow. Changes frames every 10 increments.

--- Dropper/Projectile.cs
+++ Dropper/Projectile.cs
@@ -14,7 +14,7 @@ namespace Fireball_Dodger
         private ContentManager content;
         private int v1;
         private int v2;
-        Texture2D projectileImage;
+        Texture2D projectileImage1;
         private Texture2D projectileEnergy;
         Random rndColor = new Random();
         Random rndSpawnX = new Random();
@@ -42,6 +42,13 @@ namespace Fireball_Dodger
         SoundEffectInstance energy2HitInstance;
         Rectangle powerupFlare;
         SoundEffectInstance projectHitPowerupInstance;
+        private int frameTimer;
+        private Texture2D projectileImage2;
+        private Texture2D projectileImage3;
+        private Texture2D projectileImage4;
+        private Texture2D projectileImage5;
+        private bool changeImage;
+        private int frameIncrement;
         #endregion
 
         public Projectile(Game game) : base(game)
@@ -73,11 +80,11 @@ namespace Fireball_Dodger
                 //Draws coordinates, projectiles fired (score) and the projectile itself
                 spriteBatch.DrawString(font, "Projectile Coordinates... X: " + projectileShape.X + " Y: " +
                 projectileShape.Y, new Vector2(360, 70), Color.Black);
-                spriteBatch.Draw(projectileImage, projectileShape, Color.White);
+                spriteBatch.Draw(projectileImage1, projectileShape, Color.White);
                 spriteBatch.DrawString(font, "Score: " + projectilesFired, new Vector2(450, 30), Color.Black);
 
                 //Energy ball logic
-                if (energyRoll.Next(0, 150) > 147)
+                if (energyRoll.Next(0, 25) >= 20)
                 {
                     energyProjectile = EnergyRandomRoll(energyRoll);
                 }
@@ -87,9 +94,52 @@ namespace Fireball_Dodger
                     spriteBatch.Draw(projectileEnergy, projectileShape, Color.White);
                 }
 
+                frameTimer++;
+
+                //Waits for darkenTimer to increment to 3 before switch to darker image
+                if (frameTimer >= 10)
+                {
+                    frameTimer = 0;
+
+                    if (frameIncrement == 0 || changeImage == true)
+                    {
+                        frameIncrement++;
+                        changeImage = true;
+                    }
+
+                    if (frameIncrement == 4 || changeImage == false)
+                    {
+                        changeImage = false;
+                        frameIncrement--;
+                    }
+
+                }
 
 
+                //Switches flame demon texture depending on increment value
+                switch (frameIncrement)
+                {
+                    case 0:
+                        spriteBatch.Draw(projectileImage1, projectileShape, Color.White);
+                        break;
+                    case 1:
+                        spriteBatch.Draw(projectileImage2, projectileShape, Color.White);
+                        break;
+                    case 2:
+                        spriteBatch.Draw(projectileImage3, projectileShape, Color.White);
+                        break;
+                    case 3:
+                        spriteBatch.Draw(projectileImage4, projectileShape, Color.White);
+                        break;
+                    case 4:
+                        spriteBatch.Draw(projectileImage5, projectileShape, Color.White);
+                        break;
+                }
             }
+
+
+
+        
 
 
 
@@ -151,6 +201,14 @@ namespace Fireball_Dodger
                     Console.WriteLine("new random Y projectile value: " + projectileShape.Y);
                     projectilesFired++;
 
+                    //See EnergyRandomRoll method
+                    if (energyProjectile)
+                    {
+                        projectilesFired--; //subtracts the incremented amount from the fireball
+                        projectilesFired += 3; //adds 3 points instead of 1 as a bonus
+                        energyProjectile = false;
+                    }
+
                     //Gets either div2 or div3 for the projectile size
                     if (projectileSizeRandomizer(rndColor) == "div2")
                     {
@@ -163,29 +221,10 @@ namespace Fireball_Dodger
                         projectileShape.Height = (197 / 3);
                     }
 
-                    //See EnergyRandomRoll method
-                    if (energyProjectile)
-                    {
-                        projectilesFired--; //subtracts the incremented amount from the fireball
-                        projectilesFired += 3; //adds 3 points instead of 1 as a bonus
-                        energyProjectile = false;
-                    }
+                    
 
                 }
-
-                multiCounter++;
-                Console.WriteLine("multiCounter: " + multiCounter);
-
-                multiSpawn = multiSpawner(projectileShape, rndSpawnX);
-
-                /*
-                if (multiSpawn && multiCounter > 30)
-                {
-                    Console.WriteLine("projectile Spawned");
-                    Projectile p1 = new Projectile(Game);
-                    multiCounter = 0;
-                } 
-                */
+  
 
                 if (velocity.X == 0)
                 {
@@ -238,8 +277,12 @@ namespace Fireball_Dodger
         }
 
         protected override void LoadContent()
-        {
-            projectileImage = content.Load<Texture2D>("fire");
+        {            
+            projectileImage1 = content.Load<Texture2D>(@"Fire_V3\fire_v3_1");
+            projectileImage2 = content.Load<Texture2D>(@"Fire_V3\fire_v3_2");
+            projectileImage3 = content.Load<Texture2D>(@"Fire_V3\fire_v3_3");
+            projectileImage4 = content.Load<Texture2D>(@"Fire_V3\fire_v3_4");
+            projectileImage5 = content.Load<Texture2D>(@"Fire_V3\fire_v3_5");   
             projectileEnergy = content.Load<Texture2D>("energy");
             font = content.Load<SpriteFont>("font");
             projectHit = content.Load<SoundEffect>(@"Sound\deathNoise");
@@ -268,23 +311,6 @@ namespace Fireball_Dodger
         }
 
         /// <summary>
-        /// Spawning multipe projectiles at once
-        /// </summary>
-        /// <param name="projectile"></param>
-        /// <param name="r"></param>
-        /// <returns></returns>
-        public bool multiSpawner(Rectangle projectile, Random r)
-        {
-            bool spawnNewProjectile = false;
-            if (projectile.X < r.Next(100, 300))
-            {
-                spawnNewProjectile = true;
-            }
-
-            return spawnNewProjectile;
-        }
-
-        /// <summary>
         /// Rolls a 1/25 chance to see if an energy ball is used.
         /// Energy balls give +3 points instead of +1 and are dark blue
         /// </summary>
@@ -292,9 +318,9 @@ namespace Fireball_Dodger
         private bool EnergyRandomRoll(Random energyRoll)
         {
             bool result;
-            int energyNumber = energyRoll.Next(1, 25);
+            int energyNumber = energyRoll.Next(1, 5);
 
-            if (energyNumber == 17)
+            if (energyNumber == 5)
             {
                 result = true;
             }
